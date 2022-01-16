17090fa09ccf8e4e7a3696b2cb102c771c16f4b3
EdinsonUwU <62119357+EdinsonUwU@users.noreply.github.com>
2022-01-15 19:36:20 -0500

    master

--- attendance_commands.sql
+++ attendance_commands.sql
@@ -2,19 +2,45 @@ CREATE DATABASE attendance;
 
 \c databaseName --conectarse a la base de datos
 
-CREATE TABLE [IF NOT EXISTS] table_name (
-   column1 datatype(length) column_contraint,
-   column2 datatype(length) column_contraint,
-   column3 datatype(length) column_contraint,
-   table_constraints
-);
-
 CREATE TABLE Administrador (
   admin_id VARCHAR(50),
   sede_id VARCHAR(50),
   nombre VARCHAR(50),
   contrasena VARCHAR(50),
-  PRIMARY KEY (admin_id)
+  PRIMARY KEY (admin_id)--,
+  --FOREIGN KEY (sede_id)
+      --REFERENCES Sede (sede_id)
 );
 
+CREATE TABLE Sede (
+  identificacion VARCHAR(50),
+  nombre VARCHAR(50),
+  direccion VARCHAR(50),
+  ciudad VARCHAR(50),
+  PRIMARY KEY (identificacion)
+);
+
+INSERT INTO Sede (identificacion,nombre,direccion,ciudad)
+VALUES
+  ('1','Cali','Carrera 56A No. 51 - 81','Cali'),
+  ('2','Popayán','Mayerlin Fergusson','Popayán'),
+  ('3','Popayán Norte','Carrera 56A No. 51 - 81','Popayán');
+
+\d Sede
+SELECT * FROM Sede;
+
+ALTER TABLE Administrador 
+ADD CONSTRAINT fk_Administrator 
+FOREIGN KEY (sede_id) 
+REFERENCES Sede (identificacion);
+
+
 \d Administrador
+
+INSERT INTO Administrador (admin_id,sede_id,nombre,contrasena)
+VALUES
+  ('1456','3','Carlos Hernández','12345'),
+  ('3456','2','Mayerlin Fergusson','12345'),
+  ('1456','1','Carlos Hernández','12345');
+
+SELECT * FROM Administrador;
