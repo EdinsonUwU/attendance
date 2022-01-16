de32cdfee1b1dbee5938bf1119e8fc7c843c29ac
EdinsonUwU <62119357+EdinsonUwU@users.noreply.github.com>
2022-01-16 09:16:27 -0500

    master

--- attendance_commands.sql
+++ attendance_commands.sql
@@ -12,6 +12,11 @@ CREATE TABLE Administrador (
       --REFERENCES Sede (sede_id)
 );
 
+
+
+
+--push 18
+
 CREATE TABLE Sede (
   identificacion VARCHAR(50),
   nombre VARCHAR(50),
@@ -37,10 +42,36 @@ REFERENCES Sede (identificacion);
 
 \d Administrador
 
+--error, no unicidad: solucion: añadir como pk a sede_id tambien.
 INSERT INTO Administrador (admin_id,sede_id,nombre,contrasena)
 VALUES
   ('1456','3','Carlos Hernández','12345'),
   ('3456','2','Mayerlin Fergusson','12345'),
   ('1456','1','Carlos Hernández','12345');
 
+DROP TABLE Administrador;--elimino la tabla
+
+CREATE TABLE Administrador (
+  admin_id VARCHAR(50),
+  sede_id VARCHAR(50),
+  nombre VARCHAR(50),
+  contrasena VARCHAR(50),
+  PRIMARY KEY (admin_id,sede_id)
+);
+
 SELECT * FROM Administrador;
+
+INSERT INTO Administrador (admin_id,sede_id,nombre,contrasena)
+VALUES
+  ('1456','3','Carlos Hernández','12345'),
+  ('3456','2','Mayerlin Fergusson','12345'),
+  ('1456','1','Carlos Hernández','12345');
+
+ALTER TABLE Administrador 
+ADD CONSTRAINT fk_Administrator 
+FOREIGN KEY (sede_id) 
+REFERENCES Sede (identificacion);
+
+SELECT * FROM Administrador;
+
+-- a este punto se crearon las tablas administrador y sede, cada una con su llave primaria y llave foranea.
