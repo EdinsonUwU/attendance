b2d61d42b19de949f96757897ed1f19ccafbb252
EdinsonUwU <62119357+EdinsonUwU@users.noreply.github.com>
2022-01-22 20:47:29 -0500

    master

---
+++ Front-end/App.js
@@ -0,0 +1,12 @@
+var HelloWorld = React.createClass({
+    render: function () {
+        return (
+            <h1>Attendance</h1>
+        )
+    }
+});
+
+ReactDOM.render(
+    <HelloWorld />,
+    document.getElementById('root')
+);
